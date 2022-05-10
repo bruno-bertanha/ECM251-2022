4fc012b6c8304a0625406ff1386e7e5377e450b1
bruno-bertanha <brunod.bertanha@gmail.com>
2022-05-09 22:54:20 -0300

    10 - PPT

--- 10 - PPT/src/Sistema.java
+++ 10 - PPT/src/Sistema.java
@@ -4,17 +4,19 @@ public class Sistema {
     public static void rodar(){
         Jogada jogada1 = sortearJogada();
         Jogada jogada2 = sortearJogada();
-        System.out.println("Jogada 1: " + jogada1.getTipo());
-        System.out.println("Jogada 2: " + jogada2.getTipo());
+        System.out.println("Jogada 1: " + jogada1.toString());
+        System.out.println("Jogada 2: " + jogada2.toString());
         String resultado = avaliaJogadas(jogada1, jogada2);
         System.out.println("Resultado: " + resultado);
     }
 
     private static Jogada sortearJogada(){
-        Jogada jogadas[] = new Jogada[3];
+        Jogada jogadas[] = new Jogada[5];
         jogadas[0] = new Tesoura();
         jogadas[1] = new Pedra();
         jogadas[2] = new Papel();
+        jogadas[3] = new Spock();
+        jogadas[4] = new Lizard();
         return jogadas[ThreadLocalRandom.current().nextInt(jogadas.length)];
     }
 
