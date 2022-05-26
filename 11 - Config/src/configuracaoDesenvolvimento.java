a9b094843b877fc47b5cd2457423a86695b049ba
bruno-bertanha <brunod.bertanha@gmail.com>
2022-05-26 11:33:46 -0300

    11 - Config

---
+++ 11 - Config/src/configuracaoDesenvolvimento.java
@@ -0,0 +1,23 @@
+public class configuracaoDesenvolvimento extends Configuracao{
+
+    private String user;
+
+    public configuracaoDesenvolvimento(String bANCO_DE_DADOS_URL, String aPLICACAO_URL) {
+        super(bANCO_DE_DADOS_URL, aPLICACAO_URL, true, true);
+        setUser("localhost");
+    }
+
+    @Override
+    public String getUser() {
+        return this.user;
+    }
+
+    @Override
+    protected boolean setUser(String user) {
+        if(user.isEmpty())
+            return false;
+        this.user = user;
+        return true;
+    }
+    
+}
