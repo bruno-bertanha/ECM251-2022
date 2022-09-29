96af0f01e61369ec5148441ccc2eb77ef5835a47
bruno-bertanha <brunod.bertanha@gmail.com>
2022-09-29 08:46:53 -0300

    Implementing the controller on the home page

--- projeto-carrinho/src/Home.py
+++ projeto-carrinho/src/Home.py
@@ -31,65 +31,6 @@ with col3:
             st.session_state.user = None
     else:
         st.markdown("### Please Login! Use the sidebar to the left.")
-        
-
-# Defining the products 
-products = [
-            Product(   
-                79.99,
-                "Funny Quokka T-Shirt",
-                "https://i.ibb.co/chLzdt7/tshirt.jpg",
-                """
-                This is a funny t-shirt with a quokkazilla on it.
-                It is made of 100% cotton and is machine washable.
-                It is available in the sizes: Quokka, S, M, L, XL, XXL and XXXL.
-                """,
-                ),
-            Product(
-                49.99,
-                "Quokka Socks",
-                "https://i.ibb.co/sP8rDpk/socks.jpg",
-                """
-                These are comfy socks with a quokka on it.
-                It is made of 100% cotton and is machine washable.
-                """,
-                ),
-
-            Product(
-                129.99,
-                "Quokka Plushie",
-                "https://i.ibb.co/HxB18Jt/plushie.jpg",
-                """
-                This is a cute plushie of a quokka.
-                It's perfect to comfort you during your most troubled nights.
-                """,
-                ),
-
-            Product(
-                59.99,
-                "Quokka Mug",
-                "https://i.ibb.co/LtmhQ02/mug.jpg",
-                """
-                A perfect mug to drink your coffee or tea. Since it's made of ceramic, it's safe to put it in the microwave and also the quokka will keep your liquid warm.
-                """,
-                )
-    ]
-
-# Function to display the products and add them to the cart
-def pack_product(product):
-    st.markdown(f"### {product.get_name()}")
-    st.image(f"{product.get_image()}", width=250)
-    st.markdown(f"### Investment: R$ {product.get_price()}")
-    
-    clp = st.expander("More Info")
-    clp.markdown(f"{product.get_description()}")
-    
-    add_cart = st.button("Add to cart", key=product.get_name())
-
-    if add_cart:
-        st.session_state.cart.add_product(product)
-    
-    st.write("")
 
 # Displaying the products
 if st.session_state.logged_in:
@@ -97,7 +38,7 @@ if st.session_state.logged_in:
     st.write("")
     st.write("")
     st.markdown("## Our Quokka Products")
-    for product in products:
-        pack_product(product)
+    for product in ProductController()._products:
+        ProductController.pack_product(product)
 else:
     st.error("Please login to to show our products!")
