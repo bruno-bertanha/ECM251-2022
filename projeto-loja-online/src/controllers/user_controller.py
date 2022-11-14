af4f6f86fcbb785380c54e6c3203b451d422079e
bruno-bertanha <brunod.bertanha@gmail.com>
2022-11-14 19:33:33 -0300

    Implementing UserDAO

--- projeto-loja-online/src/controllers/user_controller.py
+++ projeto-loja-online/src/controllers/user_controller.py
@@ -1,5 +1,6 @@
 from models.usuarios.user import User
 import streamlit as st
+from dao.Users_Dao import UserDAO 
 
 class UserController():
     def __init__(self):
@@ -9,6 +10,54 @@ class UserController():
             User('admin', 'admin', 'admin@gmail.com'),
         ]
     
+    def sign_up(self, user):
+        try:
+            UserDAO.get_instance().sign_up(user)
+            st.success("User created successfully!")
+        except Exception as e:
+            st.error("User could not be created!")
+            st.error(e)
+
+    def get_user(self, username):
+        try:
+            return UserDAO.get_instance().get_user(username)
+        except Exception as e:
+            st.error("User could not be retrieved!")
+            st.error(e)
+
+    def update_user_email(self, user):
+        try:
+            UserDAO.get_instance().update_user_email(user)
+            st.success("Email updated successfully!")
+        except Exception as e:
+            st.error("Email could not be updated!")
+            st.error(e)
+    
+    def update_user_password(self, user):
+        try:
+            UserDAO.get_instance().update_user_password(user)
+            st.success("Password updated successfully!")
+        except Exception as e:
+            st.error("Password could not be updated!")
+            st.error(e)
+    
+    def get_all_users(self):
+        try:
+            return UserDAO.get_instance().get_all_users()
+        except Exception as e:
+            st.error("Users could not be retrieved!")
+            st.error(e)
+    
+    
+
+
+
+
+
+
+
+
+
     def validate_user(self, phu, php):
         if st.session_state.logged_in == False:
             for user in self._users:
--- projeto-loja-online/src/dao/Users_Dao.py
+++ projeto-loja-online/src/dao/Users_Dao.py
@@ -15,3 +15,43 @@ class UserDAO:
     
     def _connect(self):
         self.conn = sqlite3.connect('./database/sqlite.db', check_same_thread=False)
+
+    def sign_up(self, user):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'INSERT INTO Users (name, email, password) VALUES (?, ?, ?)', 
+            (user.get_username(), user.get_email(), user.get_password()))
+        self.conn.commit()
+        cursor.close()
+    
+    def get_user(self, username):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'SELECT * FROM Users WHERE name = ?', 
+            (username,))
+        user = cursor.fetchone()
+        cursor.close()
+        return User(user[1], user[3], user[2])
+    
+    def update_user_email(self, user):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'UPDATE Users SET email = ? WHERE name = ?',
+            (user.get_email(), user.get_username()))
+        self.conn.commit()
+        cursor.close()
+
+    def update_user_password(self, user):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'UPDATE Users SET password = ? WHERE name = ?',
+            (user.get_password(), user.get_username()))
+        self.conn.commit()
+        cursor.close()
+    
+    def get_all_users(self):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM Users')
+        users = cursor.fetchall()
+        cursor.close()
+        return users
