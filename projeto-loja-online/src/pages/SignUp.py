ada3611e060f74c22e8ecfe902db774fc2c72526
bruno-bertanha <brunod.bertanha@gmail.com>
2022-11-15 13:13:19 -0300

    SignUp working

--- projeto-loja-online/src/pages/SignUp.py
+++ projeto-loja-online/src/pages/SignUp.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from models.product import Product
 from models.cart import Cart
+from controllers.user_controller import UserController
+from models.usuarios.user import User
 
 st.set_page_config(page_title="Quokka Store", page_icon=":moneybag:")
 
@@ -11,4 +13,9 @@ sup = st.text_input("Password", placeholder="Your password", type="password")
 
 # Login button to validate the inputs
 if st.button("Sign Up"):
-    pass
+    if sue == "" or suu == "" or sup == "":
+        st.error("Please, fill all the fields!")
+    else:
+        user = User(suu, sup, sue)
+        UserController().sign_up(user)
+        
